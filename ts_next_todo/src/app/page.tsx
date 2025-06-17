cf2a8bac1eac4232b719dd452551be60c4582d96
Marshallhch <marshallhch@gmail.com>
2025-06-17 15:23:40 +0900

    '0617_v1.0'

---
+++ ts_next_todo/src/app/page.tsx
@@ -0,0 +1,25 @@
+import { prisma } from '@/utils/prisma';
+
+async function getData() {
+  const data = await prisma.todo.findMany({
+    select: {
+      title: true,
+      id: true,
+      isCompleted: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+  return data;
+}
+
+export default async function Home() {
+  const data = await getData();
+  console.log(data);
+  return (
+    <div className="w-screen py-20 flex justify-center flex-col items-center">
+      <span>Todo App</span>
+    </div>
+  );
+}
